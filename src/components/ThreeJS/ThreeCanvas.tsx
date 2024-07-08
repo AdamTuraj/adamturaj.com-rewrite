2ef004a41c8471baebc9d4e5011f45de369f466b
Adam Turaj <adamturaj2@gmail.com>
2024-07-08 17:02:56 -0400

    Show model on screen

--- src/components/ThreeJS/ThreeCanvas.tsx
+++ src/components/ThreeJS/ThreeCanvas.tsx
@@ -2,12 +2,16 @@
 
 import { Canvas } from "@react-three/fiber";
 import PCBModel from "./PCBModel";
+import { OrbitControls } from "@react-three/drei";
 
 const ThreeCanvas = () => {
   return (
     <Canvas style={{ width: "100vw", height: "100vh" }}>
+      <OrbitControls />
+
+      <color attach="background" args={["#fff"]} />
       <ambientLight intensity={1} />
-      <PCBModel />
+      <PCBModel position={[0, 0, 0]} scale={[15, 15, 15]} />
     </Canvas>
   );
 };
